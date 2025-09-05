2cda84a9ed76ee04387e428e25258f3abc789caf
Sergei Beliaev <87997264+beliaevvc@users.noreply.github.com>
2025-09-05 06:36:46 +0500

    Update engine.ts

--- frontend/src/game/engine.ts
+++ frontend/src/game/engine.ts
@@ -100,7 +100,8 @@ export class GameEngine {
 
   async init() {
     await this.app.init({
-      background: 0x000000
+      background: 0x000000,
+      backgroundAlpha: 1,
       antialias: true,
       resizeTo: this.view,
     });
